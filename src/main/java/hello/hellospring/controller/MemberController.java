0e165cd9db0f936acef150f896536f4cd2ca3854
HyoungUkJJang <guddnr1451@gmail.com>
2021-03-08 23:03:53 +0900

    2021/03/08 commit

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,10 +1,50 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
+    private final MemberService memberService;
 
+    // 멤버 서비스 클래스에 서비스 어노테이션을 붙어줘야함 이렇게 하면 실행될때 스프링 컨테이너에서 자동으로 객체를 생성하여 관리해준다.
+    // 그전에는 그냥 단순한 자바 클래스에 불과하다. 스프링에서 알 수가 없음.
 
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm()
+    {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form)
+    {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/members")
+    public String list(Model model)
+    {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
 }
